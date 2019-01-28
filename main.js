464137982f17467d9f9570c747f7d8f156e54301
kostetr <ryabushenko2009@gmail.com>
2019-01-28 20:40:36 +0200

    коррекция

--- main.js
+++ main.js
@@ -1,26 +1,19 @@
-function btnClick() {
-    var str = '<table id=\"tableInDiv\">';
-    var prize = +document.getElementById("prize").value;
-    var member = +document.getElementById("member").value;
-    var arr = [];
-
-}
-
-
-
-function handleClick(typeGen) {    
+function handleClick(typeGen) {
     if (typeGen.value == 1) {
         var str = '<table id=\"tableInDiv\">';
-        var num = 1;
-        str += '<tr><td>' + num + '</td><td>Приз</td><td>Учасник</td></tr>';
-        num++;
-        str += '<tr><td>' + num + '</td><td><input type="text" id="prize" name="prize"></td><td><input type="text" id="member" name="member"></td></tr>';
+        str += '<tr><td>Номер</td><td>Приз</td><td>Учасник</td></tr>';
+        str += '<tr><td>qqq</td><td><input type="text" id="prize" name="prize"></td><td><input type="text" id="member" name="member"></td></tr>';
         str += '</table>';
-        str += '<input type="button" onclick="btnClick()" value="Ok">';
+        str += '<input type="button" onclick="handleClick('+typeGen.value+')" value="Ok">';
         document.getElementById("tableMendel").innerHTML = str;
+        
+        var prize = +document.getElementById("prize").value;
+        var member = +document.getElementById("member").value;
+        var arr = [];
+
+        
     } else {
         alert('Value: 2 - ' + typeGen.value);
-    }
-    ;
+    };
 }
 
