40277d158b028e592d00679c89cb0af1fcd9858e
KOCTET <ryabushenko2009@gmail.com>
2019-02-19 14:08:36 +0200

    Остались мелкие замечания

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ function handleClick(typeGen) {
         arrPrize = [];
         arrMember = [];
         oneWin = 0;
+        showMassage('', 'off');
         showTitle(oneWin);
         showTablePrizes();
         showTableMembers();
@@ -19,6 +20,7 @@ function handleClick(typeGen) {
         arrPrize = [];
         arrMember = [];
         oneWin = 1;
+        showMassage('', 'off');
         showTitle(oneWin);
         showTablePrizes();
         showTableMembers();
@@ -28,14 +30,25 @@ function handleClick(typeGen) {
 }
 function onClickPrize() {
     var prize = document.getElementById("prizeId").value;
-    arrPrize.push(prize);
-    showTablePrizes();
+    if (prize == '') {
+        showMassage('Поле приз не может быть пустым!');
+        exit();
+    } else {
+        arrPrize.push(prize);
+        showTablePrizes();
+    }
+    ;
 
 }
 function onClickMember() {
     var member = document.getElementById("memberId").value;
-    arrMember.push(member);
-    showTableMembers();
+    if (member == '') {
+        showMassage('Поле учасник не может быть пустым!');
+        exit();
+    } else {
+        arrMember.push(member);
+        showTableMembers();
+    }
 }
 function showTitle(oneWin) {
     var str = '';
@@ -52,25 +65,23 @@ function showMassage(massage, type = 0) {
         str += '<div class="alert alert-danger" role="alert">' + massage + '</div>';
     } else if (type === 1) {
         str += '<div class="alert alert-success" role="alert">' + massage + '</div>';
-    } else {
-        exit();
     }
-
+    ;
     document.getElementById("divMassage").innerHTML = str;
 }
 
 
 function showTablePrizes() {
-    var prizeStr = '<ul class="list-group"><a href="#" class="list-group-item list-group-item-action list-group-item-dark">Билет(ы):</a>';
+    var prizeStr = '<ul class="list-group"><a href="#" class="list-group-item list-group-item-action list-group-item-dark">Приз(ы):</a>';
     for (i = 0; i < arrPrize.length; i++) {
         prizeStr += '<li class="list-group-item d-flex justify-content-between align-items-center">' + arrPrize[i] + '</li>';
     }
     if (res.length == 0 && oneWin == 0) {
-        prizeStr += '<li class="list-group-item d-flex justify-content-between align-items-center"><input type="text" id="prizeId" name="prize"><input type="button" onclick="onClickPrize()" value="+"></li>';
+        prizeStr += '<li class="list-group-item d-flex justify-content-between align-items-center"><input type="text" id="prizeId" name="prize"><input type="button" class="btn btn-secondary" onclick="onClickPrize()" value="Добавить"></li>';
     } else if (res.length == 0 && oneWin == 1 && arrPrize.length == 0) {
-        prizeStr += '<li class="list-group-item d-flex justify-content-between align-items-center"><input type="text" id="prizeId" name="prize"><input type="button" onclick="onClickPrize()" value="+"></li>';
+        prizeStr += '<li class="list-group-item d-flex justify-content-between align-items-center"><input type="text" id="prizeId" name="prize"><input type="button" class="btn btn-secondary" onclick="onClickPrize()" value="Добавить"></li>';
     } else {
-        prizeStr += '<li class="list-group-item d-flex justify-content-between align-items-center"><input type="text" id="prizeId" name="prize" disabled><input type="button" onclick="onClickPrize()" value="+" disabled></li>';
+        prizeStr += '<li class="list-group-item d-flex justify-content-between align-items-center"><input type="text" id="prizeId" name="prize" disabled><input type="button" class="btn btn-secondary" onclick="onClickPrize()" value="Добавить" disabled></li>';
     }
     prizeStr += '</ul>';
     document.getElementById("prizeDiv").innerHTML = prizeStr;
@@ -81,40 +92,33 @@ function showTableMembers() {
         membersStr += '<li class="list-group-item d-flex justify-content-between align-items-center">' + (1 + i) + '   -   ' + arrMember[i] + '</li>';
     }
     if (res.length == 0) {
-        membersStr += '<li class="list-group-item d-flex justify-content-between align-items-center"><input type="text" id="memberId" name="prize"><input type="button" onclick="onClickMember()" value="+"></li>';
+        membersStr += '<li class="list-group-item d-flex justify-content-between align-items-center"><input type="text" id="memberId" name="prize"><input type="button" class="btn btn-secondary" onclick="onClickMember()" value="Добавить"></li>';
     } else {
-        membersStr += '<li class="list-group-item d-flex justify-content-between align-items-center" disabled><input type="text" id="memberId" name="prize"><input type="button" onclick="onClickMember()" value="+"disabled></li>';
+        membersStr += '<li class="list-group-item d-flex justify-content-between align-items-center"><input type="text" id="memberId" name="prize" disabled><input type="button" class="btn btn-secondary" onclick="onClickMember()" value="Добавить"disabled></li>';
     }
-
-    membersStr += '</table>';
+    membersStr += '</ul>';
     document.getElementById("membersDiv").innerHTML = membersStr;
 }
 function showTableResult() {
-    var strResultTable;
-    var resultStr = '';
+    var strResultInFile;
+    var resultStr = '<ul class="list-group"><a href="#" class="list-group-item list-group-item-action list-group-item-dark">Результаты:</a>';
     if (res.length == 0 && oneWin == 0) {
-        resultStr += '<input type="button" onclick="calcArray()" value="Mix">';
+        resultStr += '<li class="list-group-item d-flex justify-content-between align-items-center"><input type="button" class="btn btn-secondary btn-block" onclick="calcArray()" value="Mix"></li>';
     } else if (oneWin == 1 && res.length == 0) {
-        resultStr += '<input type="button" onclick="calcOneWin()" value="Mix">';
+        resultStr += '<li class="list-group-item d-flex justify-content-between align-items-center"><input type="button" class="btn btn-secondary btn-block" onclick="calcOneWin()" value="Mix"></li>';
     } else {
-        resultStr += '<table id=\"tableInDivResult\">';
-
-        resultStr += '<tr><th colspan=\"2\">Результат</th></tr>';
-        strResultTable = 'Результат\r\n';
-        strResultTable += 'Приз - Победитель;\r\n';
-
-
-        resultStr += '<tr><th>Приз:</th><th>Победитель:</th></tr>';
+        strResultInFile = 'Результат\r\n';
+        strResultInFile += 'Приз - Победитель;\r\n';
+        resultStr += '<a href="#" class="list-group-item list-group-item-action list-group-item-dark">Приз - Победитель</a>';
         for (i = 0; i < arrPrize.length; i++) {
-            resultStr += '<tr><td>' + arrPrize[i] + '</td><td>' + arrMember[res[i]] + '</td></tr>';
-            strResultTable += arrPrize[i] + ' - ' + arrMember[res[i]] + ';\r\n';
+            resultStr += '<li class="list-group-item d-flex justify-content-between align-items-center">' + arrPrize[i] + ' - ' + arrMember[res[i]] + '</li>';
+            strResultInFile += arrPrize[i] + ' - ' + arrMember[res[i]] + ';\r\n';
         }
-        resultStr += '</table>';
-        saveResult(strResultTable);
-        showMassage('Успех', 1);
+        resultStr += '</ul>';
+        saveResult(strResultInFile);
+        showMassage('Поздравляем победителей розыгрыша!', 1);
     }
-
-
+    ;
     document.getElementById("resultDiv").innerHTML = resultStr;
     showTablePrizes();
     showTableMembers();
@@ -167,20 +171,20 @@ function saveResult(str) {
 
     hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave);
     hiddenElement.target = '_blank';
-    hiddenElement.download = 'Result_' + date.yyyymmddhhmmss() + '.txt';
+    hiddenElement.download = 'Result_' + date.ddmmyyyyhhmmss() + '.txt';
     hiddenElement.click();
 }
 
-Date.prototype.yyyymmddhhmmss = function () {
+Date.prototype.ddmmyyyyhhmmss = function () {
     var mm = this.getMonth() + 1; // getMonth() is zero-based
     var dd = this.getDate();
     var hh = this.getHours();
     var min = this.getMinutes();
     var sec = this.getSeconds();
 
-    return [this.getFullYear(),
-        (mm > 9 ? '' : '0') + mm,
+    return [(mm > 9 ? '' : '0') + mm,
         (dd > 9 ? '' : '0') + dd,
+        this.getFullYear(),
         (hh > 9 ? '' : '0') + hh,
         (min > 9 ? '' : '0') + min,
         (sec > 9 ? '' : '0') + sec
